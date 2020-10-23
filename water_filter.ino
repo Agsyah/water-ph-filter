131dbf836577c5d312e915fdf366c1cf8d2307a0
thomaxxrxx <73327094+thomaxxrxx@users.noreply.github.com>
2020-10-23 11:52:34 +0700

    Update water_filter.ino

--- water_filter.ino
+++ water_filter.ino
@@ -33,8 +33,8 @@ void setup() {
   pinMode(WATER_LEVEL_MIN, INPUT);
   pinMode(WATER_LEVEL_MAX, INPUT);
 
-  pinMode(WATER_PUMP_IN, OUTPUT);
-  pinMode(WATER_PUMP_OUT, OUTPUT);
+  pinMode(WATER_PUMP_IN, INPUT);
+  pinMode(WATER_PUMP_OUT, INPUT);
 
   pinMode(WATER_PH_SENSOR, INPUT);
 
